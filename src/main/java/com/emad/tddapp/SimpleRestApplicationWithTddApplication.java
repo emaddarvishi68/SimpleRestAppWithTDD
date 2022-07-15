2284b48c281a5b41c0405ef47e9ffac53708fd46
root <darvishi.emad@gmmail.com>
2022-07-15 12:43:51 +0430

    init project

---
+++ src/main/java/com/emad/tddapp/SimpleRestApplicationWithTddApplication.java
@@ -0,0 +1,13 @@
+package com.emad.tddapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleRestApplicationWithTddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleRestApplicationWithTddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emad/tddapp/SimpleRestApplicationWithTddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emad.tddapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleRestApplicationWithTddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
